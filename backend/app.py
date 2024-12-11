992577a13f46e3321426954870f1de7508719c8f
Rares Nandra <rares@Raress-MacBook-Air.local>
2024-12-11 20:06:22 +0200

    spotify-auth 1/idk

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from Collections.User import user_bp  # Import user blueprint
-from Collections.Product import products_bp  # Import the products Blueprint
+
+from Collections.User import user_bp
+from Collections.Product import products_bp
 from Collections.Cart import cart_bp
 from Collections.Favorites import favorites_bp
+from Spotify.Auth import spotify_auth_bp
+
+from dotenv import load_dotenv
+import os
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
 
     CORS(app, resources={
@@ -18,15 +25,7 @@ def create_app():
     app.register_blueprint(products_bp)
     app.register_blueprint(favorites_bp)
     app.register_blueprint(cart_bp)
-
-    @app.route('/ping', methods=['GET'])
-    def pong():
-        data = {
-            "ping": "pong"
-        }
-        return jsonify(data)
-
-    return app
+    app.register_blueprint(spotify_auth_bp, url_prefix='/spotify/auth')
 
 
 if __name__ == '__main__':
