2fd41171e5ddc7b7ea8b4897a6420e70739643b9
Rares Nandra <rares@Raress-MacBook-Air.local>
2024-12-11 20:38:10 +0200

    spotify 2/5ish

--- backend/Spotify/Auth.py
+++ backend/Spotify/Auth.py
@@ -1,30 +1,31 @@
-from flask import Flask, redirect, request, url_for
+from flask import Flask, redirect, request, url_for, Blueprint
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 
-
 spotify_auth_bp = Blueprint('spotify-auth', __name__)
+sp_oauth = None
 
-SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
-SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
-SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+def set_spotify_auth_obj():
+    global sp_oauth
+    SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+    SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+    SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
-sp_oauth = SpotifyOAuth(
-    client_id=SPOTIPY_CLIENT_ID,
-    client_secret=SPOTIPY_CLIENT_SECRET,
-    redirect_uri=SPOTIPY_REDIRECT_URI,
-    scope="user-top-read playlist-read-private playlist-read-collaborative"
-)
+    sp_oauth = SpotifyOAuth(
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri=SPOTIPY_REDIRECT_URI,
+        scope="user-top-read playlist-read-private playlist-read-collaborative"
+    )
 
-
-@auth_bp.route('/auth', methods=['GET'])
+@spotify_auth_bp.route('/auth', methods=['GET'])
 def login():
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
 
-@auth_bp.route('/token', methods=['GET'])
+@spotify_auth_bp.route('/token', methods=['GET'])
 def callback():
     code = request.args.get('code')
     if not code:
@@ -33,6 +34,8 @@ def callback():
     try:
         token_info = sp_oauth.get_access_token(code)
         access_token = token_info['access_token']
+
+        print(access_token)
         
         # TODO write access token to DB 
 
--- backend/app.py
+++ backend/app.py
@@ -5,13 +5,16 @@ from Collections.User import user_bp
 from Collections.Product import products_bp
 from Collections.Cart import cart_bp
 from Collections.Favorites import favorites_bp
-from Spotify.Auth import spotify_auth_bp
+from Spotify.Auth import spotify_auth_bp, set_spotify_auth_obj
 
 from dotenv import load_dotenv
 import os
 
 def create_app():
-    load_dotenv()
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    env_path = os.path.join(base_dir, '.env')
+    load_dotenv(env_path)
+    set_spotify_auth_obj()
 
     app = Flask(__name__)
 
@@ -25,7 +28,8 @@ def create_app():
     app.register_blueprint(products_bp)
     app.register_blueprint(favorites_bp)
     app.register_blueprint(cart_bp)
-    app.register_blueprint(spotify_auth_bp, url_prefix='/spotify/auth')
+    app.register_blueprint(spotify_auth_bp, url_prefix='/spotify-auth')
+    return app
 
 
 if __name__ == '__main__':
